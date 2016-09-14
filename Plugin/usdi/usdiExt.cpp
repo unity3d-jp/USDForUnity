5d380380b5455a02ee9141d003845da237404d83
i-saint <saint.skr@gmail.com>
2016-09-14 20:33:37 +0900

    direct vertex buffer update worked!

--- Plugin/usdi/usdiExt.cpp
+++ Plugin/usdi/usdiExt.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 #include "usdiInternal.h"
 #include "HandleBasedVector.h"
+#include "GraphicsInterface/GraphicsInterface.h"
 
 
 namespace usdi {
@@ -31,6 +32,83 @@ typedef std::shared_ptr<TaskQueue> TaskQueuePtr;
 
 HandleBasedVector<TaskQueuePtr> g_task_queues;
 
+
+std::vector<char>& GetTemporaryBuffer()
+{
+    static thread_local std::vector<char> s_buf;
+    return s_buf;
+}
+
+
+struct vertex_v3n3
+{
+    float3 p;
+    float3 n;
+};
+
+struct vertex_v3n3u2
+{
+    float3 p;
+    float3 n;
+    float2 u;
+};
+
+template<class VertexT> static inline void WriteVertex(const usdi::MeshData& src, VertexT *dst, int i);
+
+template<> static inline void WriteVertex(const usdi::MeshData& src, vertex_v3n3 *dst, int i)
+{
+    dst[i].p = src.points[i];
+    dst[i].n = src.normals[i];
+}
+
+template<> static inline void WriteVertex(const usdi::MeshData& src, vertex_v3n3u2 *dst, int i)
+{
+    dst[i].p = src.points[i];
+    dst[i].n = src.normals[i];
+    dst[i].u = src.uvs[i];
+}
+
+template<class VertexT>
+static void WriteVertices(const usdi::MeshData& src, std::vector<char>& buf)
+{
+    using vertex_t = VertexT;
+
+    buf.resize(sizeof(vertex_t) * src.num_points);
+    vertex_t *dst = (vertex_t*)&buf[0];
+
+    for (int i = 0; i < src.num_points; ++i) {
+        WriteVertex(src, dst, i);
+    }
+}
+
+static bool UpdateVertexBuffer(const usdi::MeshData& src, void *vb, void *ib)
+{
+    auto *gi = gi::GetGraphicsInterface();
+    if (!gi) { return false; }
+
+    auto& buf = GetTemporaryBuffer();
+
+    if (vb) {
+        if (src.uvs) { WriteVertices<vertex_v3n3u2>(src, buf); }
+        else { WriteVertices<vertex_v3n3>(src, buf); }
+        gi->writeBuffer(vb, &buf[0], buf.size(), gi::BufferType::Vertex);
+    }
+
+    if (ib) {
+        // need to convert 32 bit IB -> 16 bit IB...
+        using index_t = uint16_t;
+        buf.resize(sizeof(index_t) * src.num_indices_triangulated);
+        index_t *indices = (index_t*)&buf[0];
+
+        for (int i = 0; i < src.num_indices_triangulated; ++i) {
+            indices[i] = (index_t)src.indices_triangulated[i];
+        }
+        gi->writeBuffer(vb, &buf[0], buf.size(), gi::BufferType::Index);
+    }
+
+    return true;
+}
+
 } // namespace usdi
 
 
@@ -58,7 +136,7 @@ usdiAPI bool usdiExtQueueVertexBufferUpdateTask(usdi::handle_t hq, const usdi::M
     if (!src || (!vb && !ib) || !usdi::g_task_queues.valid(hq)) { return false; }
     usdi::g_task_queues.get(hq).push([=]() {
         usdiLogTrace("vertex buffer update task\n");
-        // todo
+        usdi::UpdateVertexBuffer(*src, vb, ib);
     });
     return true;
 }
