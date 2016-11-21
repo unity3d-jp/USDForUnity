972c8e2d3d167f07fe97e723528178e7a7aeb318
i-saint <saint.skr@gmail.com>
2016-11-22 00:42:21 +0900

    included TBB binaries (as latest TBB changed license Apache 2.0) updated USD (built with latest TBB)

--- Plugin/setup.bat
+++ Plugin/setup.bat
@@ -1,15 +1,10 @@
 @echo off
 
-IF NOT EXIST "External/tbb44_20160526oss" (
-    echo "downloading TBB..."
-    Powershell.exe -executionpolicy remotesigned -File setup.ps1
-)
-
 IF NOT EXIST "External/USD" (
     cd External
     7z\7za.exe x -aos *.7z
     cd ..
     xcopy /EYF External\python27\lib\*.dll ..\USDForUnity\Assets\UTJ\Plugins\x86_64
-    xcopy /EYF External\tbb44_20160526oss\bin\intel64\vc14\tbb.dll ..\USDForUnity\Assets\UTJ\Plugins\x86_64
+    xcopy /EYF External\tbb\lib\tbb.dll ..\USDForUnity\Assets\UTJ\Plugins\x86_64
     xcopy /EYF External\USD\lib\*.dll ..\USDForUnity\Assets\StreamingAssets\UTJ\USDForUnity\plugins\lib
 )
--- Plugin/setup.ps1
+++
@@ -1,9 +0,0 @@
-Add-Type -AssemblyName System.IO.Compression.FileSystem
-function Unzip
-{
-    param([string]$zipfile, [string]$outpath)
-    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
-}
-
-wget "https://www.threadingbuildingblocks.org/sites/default/files/software_releases/windows/tbb44_20160526oss_win_0.zip" -OutFile External/tbb44_20160526oss_win_0.zip
-Unzip "External/tbb44_20160526oss_win_0.zip" "External"
