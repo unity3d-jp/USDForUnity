unity3d-jp/USDForUnity

commit d12717ef2ed84d8bd7681d882cfafd3add93234f

fixed potential deadlock

Plugin/usdi/ext/usdiTask.h
@@ -88,6 +88,7 @@ class Task
 {
 public:
     Task(const std::function<void()>& f, const char *n = "");
+    ~Task();
     void run(bool async = true);
     bool isRunning();
     void wait();
@@ -95,11 +96,9 @@ public:
     usdiDefineCachedOperatorNew(Task, 256);
 
 private:
-    static tbb::task_group s_task_group;
-
     std::string m_dbg_name;
     std::function<void()> m_func;
-    tbb::spin_mutex m_mutex;
+    tbb::mutex m_mutex;
 };
 
 } // namespace usdi

Plugin/usdi/ext/usdiTask.cpp
@@ -220,7 +220,27 @@ void VertexCommandManager::wait()
 
 
 
-tbb::task_group Task::s_task_group;
+template<typename Body>
+class lambda_task : public tbb::task
+{
+private:
+    Body m_body;
+    tbb::task* execute() override
+    {
+        m_body();
+        return nullptr;
+    }
+public:
+    lambda_task(const Body& body) : m_body(body) {}
+};
+
+template<typename Body>
+inline tbb::task* launch(const Body& body)
+{
+    auto *ret = new(tbb::task::allocate_root()) lambda_task<Body>(body);
+    tbb::task::enqueue(*ret);
+    return ret;
+}
 
 Task::Task(const std::function<void()>& f, const char *n)
     : m_dbg_name(n)
@@ -228,19 +248,21 @@ Task::Task(const std::function<void()>& f, const char *n)
 {
 }
 
+Task::~Task()
+{
+}
+
 void Task::run(bool async)
 {
     if (async) {
         m_mutex.lock();
-        s_task_group.run([this]() {
+        launch([this]() {
             m_func();
             m_mutex.unlock();
         });
     }
     else {
-        m_mutex.lock();
         m_func();
-        m_mutex.unlock();
     }
 }
 

commit 9d27c32c220bb9c8617bf7627d5ca0ebc5cba0fd

fixed sync bug (introduced in d12717ef2ed84d8bd7681d882cfafd3add93234f)

Plugin/usdi/ext/usdiTask.h
@@ -99,7 +99,7 @@ public:
 private:
     std::string m_dbg_name;
     std::function<void()> m_func;
-    tbb::mutex m_mutex;
+    tbb::spin_mutex m_mutex; // must be non-recursive mutex
 };
 
 } // namespace usdi